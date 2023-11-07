f7161c493d0b2dd31d93aefae1827f6cc6eced29
earth774 <sutthiphongnuanma@gmail.com>
2023-11-07 23:37:38 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {useStorage} from '@/composables/useStorage';
+
+ let food = useStorage('food','salad')
+</script>
+
+<template>
+  <main>
+    <p>
+      What is your favorite food? <input type="text" v-model="food" >
+    </p>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,58 @@
+<script setup>
+  import {useFlash} from "@/composables/useFlash";
+
+  let {flash} = useFlash();
+</script>
+
+
+<!-- <script>
+import flash from '@/mixins/flash';
+
+export default{
+  mixins:[flash]
+};
+</script> -->
+
+ <!-- <script setup>
+//import TheWelcome from '../components/TheWelcome.vue' 
+
+// import { onMounted,ref } from 'vue';
+// export default{
+  // components:{TheWelcome},
+
+  // setup() {
+  //   onMounted(() => {
+  //     alert("app alraedy");
+  //   });
+
+  //   let message = ref("Hello World");
+
+  //   setTimeout(() => {
+  //     message.value = "new set";
+  //   }, 2000);
+
+  //   return{
+  //     message
+  //   }
+  // }
+
+  // eslint-disable-next-line no-undef
+// let message = $ref("Hello World 123");
+
+// setTimeout(() => {
+//   message = "new set";
+// }, 2000);
+
+// let doYouClick = ()=>{
+//   alert('abc');
+// };
+// };
+</script> -->
+
+<template>
+  <main>
+    <TheWelcome />
+    <h1>{{ message }}</h1>
+    <button @click="flash('Success!','Hello World')">Click</button>
+  </main>
+</template>
